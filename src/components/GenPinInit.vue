5ad64691aca22cfb1684cc7c29a3fadb4bf68e87
ZHU Hao <hao_kr@163.com>
2022-07-30 14:40:00 +0800

    加上引脚选择的判断

--- src/components/GenPinInit.vue
+++ src/components/GenPinInit.vue
@@ -1,17 +1,20 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { ElMessage, ElMessageBox } from 'element-plus'
 
 let pinId = 0;
 export default defineComponent({
     data() {
         return {
-            port: "GPIOA",
-            pinTables: [
-                { id: pinId++, name: "pin0", checked: false },
-            ],
-            pinNumber: 16,
+            pinCfg: {
+                port: "GPIOA",
+                pinTables: [
+                    { id: pinId++, name: "pin0", checked: false },
+                ],
+                pinNumber: 16,
+                pinDefaultStateIsLow: false,
+            },
             pinIsUpdate: false,
-            pinDefaultStateIsLow: false,
             result: "未检测到引脚",
             dialogTableVisible: false,
             textarea2: "",
@@ -19,15 +22,15 @@ export default defineComponent({
     },
     methods: {
         updatePinTables() {
-            if (pinId < this.pinNumber) {
-                for (var i = pinId; i < this.pinNumber; i++) {
-                    this.pinTables.push({ id: pinId++, name: "pin" + i.toString(), checked: false });
+            if (pinId < this.pinCfg.pinNumber) {
+                for (var i = pinId; i < this.pinCfg.pinNumber; i++) {
+                    this.pinCfg.pinTables.push({ id: pinId++, name: "pin" + i.toString(), checked: false });
                 }
             }
         },
         clearPinSelection() {
-            for (var i = 0; i < this.pinNumber; i++) {
-                this.pinTables[i].checked = false;
+            for (var i = 0; i < this.pinCfg.pinNumber; i++) {
+                this.pinCfg.pinTables[i].checked = false;
             }
         },
         genCodeEnd() {
@@ -39,32 +42,38 @@ export default defineComponent({
                 this.pinIsUpdate = false; // 避免重复数据
 
                 var pinCode = "(" + this.result + ")";
-                var portFen = this.port + "FEN" + " &= ~" + pinCode + ";\n";
-                var portDe = this.port + "DE" + " |= " + pinCode + ";\n";
-                var portDir = this.port + "DIR" + " &= ~" + pinCode + ";\n";
+                var portFen = this.pinCfg.port + "FEN" + " &= ~" + pinCode + ";\n";
+                var portDe = this.pinCfg.port + "DE" + " |= " + pinCode + ";\n";
+                var portDir = this.pinCfg.port + "DIR" + " &= ~" + pinCode + ";\n";
                 var port = "";
-                if (this.pinDefaultStateIsLow == false) {
-                    port = this.port + " |= " + pinCode + ";\n";
+                if (this.pinCfg.pinDefaultStateIsLow == false) {
+                    port = this.pinCfg.port + " |= " + pinCode + ";\n";
                 } else {
-                    port = this.port + " &= ~" + pinCode + ";\n";
+                    port = this.pinCfg.port + " &= ~" + pinCode + ";\n";
                 }
                 this.result = portFen + portDe + portDir + port;
             }
         },
         genCode() {
-            this.genCodeDo();
-            this.dialogTableVisible = true;
+            if (this.pinIsUpdate == false && this.result == "") {
+                ElMessageBox.alert('请先选择引脚', '注意！', {
+                    confirmButtonText: 'Close',
+                })
+            } else {
+                this.genCodeDo();
+                this.dialogTableVisible = true;
+            }
         }
     },
     mounted() {
         this.updatePinTables();
     },
     watch: {
-        pinTables: {
+        pinCfg: {
             handler(val) {
                 var selectedPin = "";
-                for (var i = 0; i < this.pinNumber; i++) {
-                    if (this.pinTables[i].checked == true) {
+                for (var i = 0; i < this.pinCfg.pinNumber; i++) {
+                    if (this.pinCfg.pinTables[i].checked == true) {
                         selectedPin += "BIT(" + i.toString() + ")" + " | ";
                     }
                 }
@@ -94,10 +103,10 @@ export default defineComponent({
 
             <el-space direction="horizontal">
                 <el-tag size="large">默认引脚电平选择</el-tag>
-                <el-switch v-model="pinDefaultStateIsLow" size="large" active-text="低电平" inactive-text="高电平" />
+                <el-switch v-model="pinCfg.pinDefaultStateIsLow" size="large" active-text="低电平" inactive-text="高电平" />
             </el-space>
 
-            <el-radio-group v-model="port" size="large">
+            <el-radio-group v-model="pinCfg.port" size="large">
                 <el-radio-button label="GPIOA" />
                 <el-radio-button label="GPIOB" />
                 <el-radio-button label="GPIOE" />
@@ -106,16 +115,16 @@ export default defineComponent({
 
             <el-space direction="horizontal">
                 <div :key="1">
-                    <div v-for="(pin, index) in pinTables" :key="pin.id" class="text item">
-                        <div v-if="index < pinNumber / 2">
+                    <div v-for="(pin, index) in pinCfg.pinTables" :key="pin.id" class="text item">
+                        <div v-if="index < pinCfg.pinNumber / 2">
                             <el-checkbox :label="pin.id.toString" v-model="pin.checked" border>{{ pin.name }}
                             </el-checkbox>
                         </div>
                     </div>
                 </div>
                 <div :key="2">
-                    <div v-for="(pin, index) in pinTables" :key="pin.id" class="text item">
-                        <div v-if="index > pinNumber / 2 - 1">
+                    <div v-for="(pin, index) in pinCfg.pinTables" :key="pin.id" class="text item">
+                        <div v-if="index > pinCfg.pinNumber / 2 - 1">
                             <el-checkbox :label="pin.id.toString" v-model="pin.checked" border>{{ pin.name }}
                             </el-checkbox>
                         </div>
@@ -125,8 +134,7 @@ export default defineComponent({
 
             <el-dialog v-model="dialogTableVisible" title="代码预览">
 
-                <el-input v-model="result" :autosize="{ minRows: 2, maxRows: 4 }" type="textarea"
-                    placeholder="未选择引脚" />
+                <el-input v-model="result" :autosize="{ minRows: 2, maxRows: 4 }" type="textarea" placeholder="未选择引脚" />
 
                 <template #footer>
                     <span class="dialog-footer">
