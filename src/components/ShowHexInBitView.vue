348decd1f37b538853b5ded8c6dfbfaea30edf8b
ZHU Hao <hao_kr@163.com>
2022-08-03 11:43:49 +0800

    重命名

--- src/components/ShowHexInBitView.vue
+++ src/components/ShowHexInBitView.vue
@@ -1,19 +1,19 @@
 <script lang="ts" setup>
 import { nextTick, ref, watch } from 'vue'
-const inHex = ref('')
+const inputHex = ref('')
 
-watch(inHex, (newValue, oldValue) => {
+watch(inputHex, (newValue, oldValue) => {
     const newValueSplit = newValue.split('');
     const result = newValueSplit.filter(c => (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F')).join('');
 
     nextTick(() => {
-        inHex.value = result.substring(0, 8);
+        inputHex.value = result.substring(0, 8);
     });
 });
 </script>
 
 <template>
     <el-card class="box-card" style="width: 400px">
-        <el-input v-model="inHex" placeholder="Please input" />
+        <el-input v-model="inputHex" placeholder="Please input" />
     </el-card>
 </template>
