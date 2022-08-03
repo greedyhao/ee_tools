96aaca39e601b9cc68165dd72d6246df5f7c2e50
ZHU Hao <hao_kr@163.com>
2022-08-03 10:56:27 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,20 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 
+import { emit } from '@tauri-apps/api/event'
+import { listen } from '@tauri-apps/api/event'
+
+emit('tauri://update')
+
+listen('tauri://update-available', function (res) {
+  console.log('New version available: ', res)
+  emit('tauri://update-install')
+})
+
+listen('tauri://update-status', function (res) {
+  console.log('New status: ', res)
+})
+
 export default {
     data() {
         return {
