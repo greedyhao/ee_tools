f5558d7e8f7b4a35c477ad20998297471c1a46e6
ZHU Hao <hao_kr@163.com>
2022-08-10 00:02:01 +0800

    异常信息转换页面 换开关

--- src/components/ConverExceptionLog.vue
+++ src/components/ConverExceptionLog.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 const textarea = ref('')
+const showRawData = ref(true)
 </script>
 
 <template>
     <el-card class="box-card" style="width: 600px">
         <el-input v-model="textarea" rows="14" type="textarea" placeholder="Please input" />
     </el-card>
-    <el-row>
-        <el-button>转换</el-button>
-    </el-row>
+    <el-switch v-model="showRawData" size="default" active-text="原数据" inactive-text="转换后" />
 
 </template>
