5178adedadf4d73dfdcd4ae511ba26b5929b14f3
ZHU Hao <hao_kr@163.com>
2022-08-13 10:58:38 +0800

    删掉 router

--- src/components/ConverExceptionLog.vue
+++ src/components/ConverExceptionLog.vue
@@ -3,11 +3,6 @@ import { ref, computed } from 'vue'
 import ConverExceptionLogAfter from './ConverExceptionLogAfter.vue';
 import ConverExceptionLogBefore from './ConverExceptionLogBefore.vue';
 
-const routes = {
-    '/': ConverExceptionLogBefore,
-    '/about': ConverExceptionLogAfter
-}
-
 const textarea = ref('')
 const showRawData = ref(true)
 
@@ -31,9 +26,8 @@ export default {
 </script>
 
 <template>
-    <component :is="currentView" />
-    <el-space :size="10" spacer="|">
-        <el-link href="#/">处理前</el-link>
-        <el-link href="#/about">处理后</el-link>
+    <el-space direction="vertical">
+        <ConverExceptionLogAfter />
+        <ConverExceptionLogBefore />
     </el-space>
 </template>
